77beb380ee09e492781add31c40acfc12b584512
rizkyy12 <rizky.andhika@mhs.polman-bandung.ac.id>
2021-04-19 10:07:20 +0700

    update motor

--- motor/motor.ino
+++ motor/motor.ino
@@ -2,36 +2,26 @@
 #include <geometry_msgs/Twist.h>
 
 //MOTOR1
-#define RPWM1 4
-#define LPWM1 5
+#define RPWM1 5
+#define LPWM1 4
+#define LED 13
 
 float Vm_x, Vm_y, Vm_z;
 float V1, V2, V3;
 
 ros::NodeHandle nh;
 
-void motorCb(const geometry_msgs::Twist& msg){
+void motorCb(const geometry_msgs::Twist& msg){  
   Vm_x = msg.linear.x;
   Vm_y = msg.linear.y;
   Vm_z = msg.linear.z;
 
   V1 = (-Vm_x / 2) - (sqrt(3)*Vm_y/2) + Vm_z;
-  
-  if (V1 >= 0){
-    digitalWrite(RPWM1, V1);
-    digitalWrite(LPWM1, 0);
-  }
-  else {
-    digitalWrite(RPWM1, 0);
-    digitalWrite(LPWM1, -V1);
-  }
-  
 }
 
 ros::Subscriber<geometry_msgs::Twist> sub("/cmd_vel", &motorCb);
 
 void setup() {
-
   pinMode (RPWM1, OUTPUT);
   pinMode (LPWM1, OUTPUT);
   nh.initNode();
@@ -39,6 +29,16 @@ void setup() {
 }
 
 void loop() {
+  if (V1 >= 0){
+    analogWrite(RPWM1, V1);
+    analogWrite(LPWM1, 0);
+    digitalWrite(LED, HIGH);
+  }
+  else {
+    analogWrite(RPWM1, 0);
+    analogWrite(LPWM1, -V1);
+    digitalWrite(LED, LOW);
+  }
   nh.spinOnce();
   delay(1);
 }
